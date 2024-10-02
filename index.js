d3601832cb6d41919b526e9f3dccbac33d5344a6
Dnica98 <daniela.nica98@gmail.com>
2024-10-02 16:32:07 +0300

    1

---
+++ index.js
@@ -0,0 +1,92 @@
+// const num = 9;
+// console.log('1.type of num is:', typeof num)
+
+// const str = 'string';
+// console.log('2.type of str is:', typeof str)
+
+// const bool = false;
+// console.log('3.type of bool is:', typeof bool)
+
+// const undief = undefined;
+// console.log('4.type of undief is:', typeof undief)
+
+// const nul = null;
+// console.log('5.type of nul is:', typeof nul)
+
+// const symb = Symbol;
+// console.log('6.type of symb is:', typeof symb)
+
+// const bigInt = BigInt(10);
+// console.log('7.type of bigInt is:', typeof bigInt )
+
+// const obj = {2: 1,};
+// console.log('8.type of obj is:', typeof obj)
+
+
+// // ex.4 (adunare, scarede si coeziune)
+// const num1 = 5;
+// const num2 = 9;
+// const num3 = 2;
+// const str1 = 'mere';
+// const str2 ='si';
+// const str3 = 'pere';
+// const str4 = ' ';
+
+// console.log(num1+num2+num3)
+// console.log(num1-num2-num3)
+// console.log(str1+str4+str2+str4+str3)
+// console.log(num1+num3+str4+str3)
+// console.log(num1+str4+str1+str4+str2+str4+num2+str4+str3)
+
+
+// // ex.3(equal si strong equal)
+// console.log(0 == '')
+// console.log(0 === '')
+// console.log(65 >= 35)
+// console.log(101 <= 4)
+// console.log(55 > 78)
+// console.log(78 < 4557)
+// console.log(66 != 66)
+// console.log(66 !== 66)
+
+// // ex.3(negare)
+// const falsy1 = 0;
+// const falsy2 = '';
+// const falsy3 = undefined;
+// const falsy4 = null;
+// const falsy5 = NaN;
+
+// console.log(!falsy1)
+// console.log(!falsy2)
+// console.log(!falsy3)
+// console.log(!falsy4)
+// console.log(!falsy5)
+
+// console.log(!!falsy1)
+// console.log(!!falsy2)
+// console.log(!!falsy3)
+// console.log(!!falsy4)
+// console.log(!!falsy5)
+
+
+// ex.3 (++, --)
+let num1 = 15;
+
+console.log(++num1)
+console.log(--num1)
+
+let dcr = 8;
+
+console.log(--dcr)
+
+num1=num1-dcr;
+
+console.log(num1-dcr)
+
+
+let num3 = 2
+let num4 = 5
+
+console.log(num3*num4)
+console.log(num3**num4)
+console.log(num4/num3)
